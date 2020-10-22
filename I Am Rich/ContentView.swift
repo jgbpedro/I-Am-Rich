249ae4c7277b3e53b837da37189394524e91e4a0
jgbpedro <jguilhermebp@live.com>
2020-10-22 20:31:46 -0300

    Add files via upload

---
+++ I Am Rich/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  I Am Rich
+//
+//  Created by José Guilherme on 15/10/20.
+//  Copyright © 2020 José Guilherme. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
